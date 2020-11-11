bfb18c2ed447660bb6197510b06940cffece56c1
moonchild <1792895442@qq.com>
2020-11-11 23:30:42 +0800

    加功能

--- app/src/main/java/com/example/calculator_first/MainActivity.java
+++ app/src/main/java/com/example/calculator_first/MainActivity.java
@@ -11,4 +11,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void onClick(View view){
+        switch(view.getId()){
+            case R.id.btn_0:
+            case R.id.btn_1:
+            case R.id.btn_2:
+            case R.id.btn_3:
+            case R.id.btn_4:
+            case R.id.btn_5:
+            case R.id.btn_6:
+            case R.id.btn_7:
+            case R.id.btn_8:
+            case R.id.btn_9:
+            case R.id.btn_add:
+            case R.id.btn_minus:
+            case R.id.btn_time:
+            case R.id.btn_div:
+            case R.id.btn_dot:
+            case R.id.btn_ac:
+            case R.id.btn_equal:
+            case R.id.btn_qy:
+            case R.id.btn_zf:
+                break;
+
+        }
+
+    }
 }

ea32817dbfb06b30d9a711d6f3ca76a808e6f9fb
moonchild <1792895442@qq.com>
2020-11-12 23:20:49 +0800

    加功能加功能

--- app/src/main/java/com/example/calculator_first/MainActivity.java
+++ app/src/main/java/com/example/calculator_first/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.calculator_first;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
